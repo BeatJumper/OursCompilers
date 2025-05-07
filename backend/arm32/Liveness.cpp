245e7d289657fb0907089b38d4cd6c484018f074
lqm_22 <lqm_22@mail.nwpu.edu.cn>
2025-05-08 01:33:35 +0800

    干涉图

--- backend/arm32/Liveness.cpp
+++ backend/arm32/Liveness.cpp
@@ -10,12 +10,15 @@ bool update_live(Node_Dataflow * node, Node_Dataflow * succ1, Node_Dataflow * su
     if (succ2) {
         ret |= merge_set(node->liveOUT, succ2->liveIN);
     }
+    return ret;
 }
 void LiveVariableAnalysis(ControlFlowGraph * _graph)
 {
     auto & node_list = _graph->get_node_list();
     bool need_update = true;
     while (need_update) {
+
+        // 当且仅当LIVEOUT在迭代后发生更改才继续迭代
         need_update = false;
         for (Node_CFG * node: node_list) {
             auto & dataflow_list = node->get_dataflow_list();
@@ -38,6 +41,13 @@ void LiveVariableAnalysis(ControlFlowGraph * _graph)
     }
 }
 
+/// @brief 集合合并的工具函数
+/// @tparam T 集合内元素类型
+/// @param a 集合a
+/// @param b 集合b
+/// @return 并入后集合a是否并入了新值
+/// @note 把集合b的元素逐个加入集合a
+/// @note 时间复杂度：O(Blog(A+B)) 其中A，B分别代表集合a和b的大小
 template <typename T>
 bool merge_set(std::set<T> & a, std::set<T> & b)
 {
@@ -48,6 +58,11 @@ bool merge_set(std::set<T> & a, std::set<T> & b)
     return a.size() == size0;
 }
 
+/// @brief 集合求差集的工具函数
+/// @tparam T 集合内元素类型
+/// @param a 集合a
+/// @param b 集合b
+/// @return 作差后的集合
 template <typename T>
 std::set<T> set_difference(std::set<T> & a, std::set<T> & b)
 {
@@ -62,9 +77,44 @@ std::set<T> set_difference(std::set<T> & a, std::set<T> & b)
 
 node_IG::node_IG(Value * _val) : val(_val)
 {}
-InterferenceGraph::InterferenceGraph(Node_CFG * node_cfg)
+
+void node_IG::add_neighbor(node_IG * neighbor)
 {
-    for (Value * val: node_cfg->get_value_list()) {
-        node_list.push_back(new node_IG(val));
+    neighbors.insert(neighbor);
+}
+
+void InterferenceGraph::add_edge(node_IG * node1, node_IG * node2)
+{
+    node1->add_neighbor(node2);
+    node2->add_neighbor(node1);
+}
+
+InterferenceGraph::InterferenceGraph(ControlFlowGraph * graph)
+{
+
+    // 从Value到干涉图节点的映射
+    std::map<Value *, node_IG *> value_to_ig;
+
+    for (Value * val: graph->get_value_list()) {
+        node_IG * newnode = new node_IG(val);
+        value_to_ig[val] = newnode;
+        node_list.push_back(newnode);
+    }
+
+    //扫描函数里每条指令，获取每个时刻的活跃变量集合
+    for (Node_CFG * node_cfg: graph->get_node_list()) {
+        for (Node_Dataflow * node_data: node_cfg->get_dataflow_list()) {
+
+            // 某个指令位置下活跃着的量的集合
+            std::set<Value *> value_occupy = node_data->liveOUT;
+            merge_set(value_occupy, node_data->def_set);
+
+            // 这些不同的量两两之间都是互斥的，不能在同一寄存器
+            FOR_EACH_PAIR_IN_SET(value_occupy)
+            {
+                // 在干涉图中连上一条边
+                add_edge(value_to_ig[*it1], value_to_ig[*it2]);
+            }
+        }
     }
 }
--- backend/arm32/Liveness.h
+++ backend/arm32/Liveness.h
@@ -1,6 +1,9 @@
-#include <queue>
-
 #include "CFG.h"
+
+// 定义宏来遍历 set 中的二元组合
+#define FOR_EACH_PAIR_IN_SET(set)                                                                                      \
+    for (auto it1 = (set).begin(); it1 != (set).end(); ++it1)                                                          \
+        for (auto it2 = std::next(it1); it2 != (set).end(); ++it2)
 
 /// @brief 对一个数据流节点更新LIVEIN和LIVEOUT
 /// @param node 要更新的数据流节点
@@ -17,8 +20,9 @@ void LiveVariableAnalysis(ControlFlowGraph * _graph);
 /// @tparam T 集合内元素类型
 /// @param a 集合a
 /// @param b 集合b
-/// @return 并入后集合a是否发生变化
-/// @note 把集合b的元素全加入集合a
+/// @return 并入后集合a是否并入了新值
+/// @note 把集合b的元素逐个加入集合a
+/// @note 时间复杂度：O(Blog(A+B)) 其中A，B分别代表集合a和b的大小
 template <typename T>
 bool merge_set(std::set<T> & a, std::set<T> & b);
 
@@ -32,21 +36,28 @@ std::set<T> set_difference(std::set<T> & a, std::set<T> & b);
 
 /// @brief 干涉图的节点
 struct node_IG {
-    /// @brief 干涉图节点对应的量
+    /// @brief 干涉图节点对应的Value
     Value * val;
     /// @brief 干涉图节点的邻接表
-    std::set<node_IG *> next_nodes;
+    std::set<node_IG *> neighbors;
     /// @brief 构造函数
-    /// @param _val 节点的值
+    /// @param _val 节点对应的Value
     node_IG(Value * _val);
+    /// @brief 给干涉图节点添加邻居
+    /// @param neighbor 邻居的指针
+    void add_neighbor(node_IG * neighbor);
 };
 /// @brief 干涉图
 struct InterferenceGraph {
     /// @brief 干涉图中的节点列表
     std::vector<node_IG *> node_list;
-    /// @brief 对一个活跃分析后的基本块创建干涉图
-    /// @param node_cfg 基本块在CFG中的节点
-    InterferenceGraph(Node_CFG * node_cfg);
+    /// @brief 干涉图中添加一条无向边
+    /// @param node1 干涉图的一个节点
+    /// @param node2 干涉图的另一个节点
+    static void add_edge(node_IG * node1, node_IG * node2);
+    /// @brief 对一个活跃分析后的控制流图创建干涉图
+    /// @param graph 控制流图
+    InterferenceGraph(ControlFlowGraph * graph);
     /// @brief 析构函数
     ~InterferenceGraph();
 };
